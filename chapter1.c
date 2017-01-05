afb78c03d2d1a4ab141fd2bfd70b7bc80670b2b6
Lemmon0402 <lemmon0402@gmail.com>
2017-01-05 18:32:36 +0800

    add chapter1

---
+++ chapter1.c
@@ -0,0 +1,57 @@
+/*************************************************************************
+	> File Name: chpter1.c
+	> Author: li 
+	> Mail: lemmon0402@gmail.com
+	> Created Time: Thu 05 Jan 2017 03:12:46 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<limits.h>
+/*整数取值范围
+int main()
+{
+    int i;
+    i = INT_MAX - 1;
+    printf("%d\n", i);
+    i++;
+    printf("%d\n", i);
+    i++;
+    printf("%d\n", i);
+    return 0;
+}
+*/
+
+/*三角形输出
+int main()
+{
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("****\n");
+    printf("*****\n");
+}
+*/
+
+/*加框文字输出*/
+
+/*整数四则运算
+int main()
+{
+    int x, y;
+    scanf("%d%d", &x, &y);
+    printf("%d+%d=%d", x,y,x*y);
+}
+*/
+
+/*浮点数精度
+int main()
+{
+    float x;
+    double y;
+    x = 12345.67890;
+    y = 12345.67890;
+    printf("x = %f\ny = %f\n", x, y);
+    printf("x = %f\ny = %lf\n", x, y);
+    printf("x = %lf\ny = %lf\n", x, y);
+}
+*/
