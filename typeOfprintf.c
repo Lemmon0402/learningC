b5137d5c63890e17ee7ffd6bb1b02efa16fecbe2
lemmon <pm0530@163.com>
2017-12-04 19:38:36 +0800

    a practice of printf

---
+++ typeOfprintf.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: typeOfprintf.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2017年12月04日 星期一 19时32分27秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+    char c = 'x';
+    char s[] = "dsfjlajgiejaljewoahjkl";
+    
+    printf("Integers:\n");
+}
